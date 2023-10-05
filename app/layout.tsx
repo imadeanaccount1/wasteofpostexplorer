6d14c5640f20d818559dbd28670914985b21e482
imadeanaccount1 <imadeanaccount@proton.me>
2023-10-05 13:38:42 -0400

    fix home link

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ export const metadata = {
   description: 'Find, filter, search, sort, and paginate scraped wasteof posts and users.',
 }
 
+import Head from 'next/head'
+
 export default function RootLayout({
   children,
 }: {
@@ -10,6 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+      <meta
+          property="og:image"
+          content="./image.jpg"
+        />
+      </Head>
       <body>{children}</body>
     </html>
   )
