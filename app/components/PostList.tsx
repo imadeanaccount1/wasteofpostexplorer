37dec45a628c71ab610b6f5775e8916631d6eae7
8Bit <75950907+8BitOSC@users.noreply.github.com>
2023-10-12 18:45:25 -0500

    add option! wow! incredible!

--- app/components/PostList.tsx
+++ app/components/PostList.tsx
@@ -214,6 +214,7 @@ function SortBy(props: any) {
           <Option value="loves">Love Count</Option>
           <Option value="reposts">Repost Count</Option>
           <Option value="comments">Comment Count</Option>
+          <Option value="edits">Edit Count</Option>
         </Select>
         <Typography
           sx={{ alignSelf: "center" }}
