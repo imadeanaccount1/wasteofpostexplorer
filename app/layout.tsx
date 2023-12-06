65f9bf0eb14be4e1f24fb3a5c8ef387fc8a7e4db
imadeanaccount1 <imadeanaccount@proton.me>
2023-12-06 14:31:00 -0500

    ra

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      {/* <script async src="https://postexplorer-umami-imadeanaccount.vercel.app/script.js" data-website-id="21c969ed-14e3-450f-bb08-8ee748fe8590"></script> */}
+      <script async src="https://postexplorer-umami-imadeanaccount.vercel.app/script.js" data-website-id="21c969ed-14e3-450f-bb08-8ee748fe8590"></script>
       </head>
       <body>{children}</body>
     </html>
